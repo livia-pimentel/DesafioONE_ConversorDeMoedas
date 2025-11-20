c5dfcb88147dcbc80be356616ca47c96ca385d86
livia-pimentel <livia_macedo89@yahoo.com.br>
2025-11-20 17:02:05 -0300

    Add classe para responsavel pelas requisções

---
+++ src/ConsultExchange.java
@@ -0,0 +1,29 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultExchange {
+
+    public ResponseExchange ConsultExchange(String currency_code) {
+        // Cria a variavel e coloca o nome que foi salvo em vaiaveis de ambiente
+        String apiKey = System.getenv("API_KEY");
+
+        URI address = URI.create("https://v6.exchangerate-api.com/v6/" + apiKey +"/latest/" + currency_code);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(address)
+                .build();
+
+        try {
+            HttpResponse<String> response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), ResponseExchange.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Moeda não encontrada.");
+        }
+    }
+}
