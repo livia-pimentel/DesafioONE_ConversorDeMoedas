2827786a31c2e18841cf1e37a1a196a46cd7009b
livia-pimentel <livia_macedo89@yahoo.com.br>
2025-11-21 09:30:47 -0300

    Atualização dos componentes do record

--- src/ResponseExchange.java
+++ src/ResponseExchange.java
@@ -1,4 +1,10 @@
 import java.util.Map;
 
-public record ResponseExchange(String base_code, Map<String, Double> conversion_rates) {
-}
+/* Os componenetes (que servem tanto como atributos como metodos no record),
+devem ter o mesmo nome que consta no retorno da API
+ */
+public record ResponseExchange(
+        String base_code,
+        String time_last_update_utc,
+        Map<String, Double> conversion_rates
+) {}
